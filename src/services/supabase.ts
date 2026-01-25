af29036ab2a272f60efa69aab4b5de41d20bfd65
Jake <jakeorton99@gmail.com>
2026-01-25 18:00:13 +0200

    ADD: typed supabase create client leading to all return data to be typed

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,14 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 import { appConfig } from "../config";
+import { Database } from "../types/supabase";
 
 // Create a basic client for public operations (like reading locations)
-export const supabaseClient = createClient(
+export const supabaseClient = createClient<Database>(
   appConfig.supabase.url,
-  appConfig.supabase.anonKey
+  appConfig.supabase.anonKey,
 );
 
 // Admin client for protected operations that bypass RLS
-export const supabaseAdmin = createClient(
+export const supabaseAdmin = createClient<Database>(
   appConfig.supabase.url,
   appConfig.supabase.serviceKey,
   {
@@ -16,5 +17,5 @@ export const supabaseAdmin = createClient(
       autoRefreshToken: false,
       persistSession: false,
     },
-  }
+  },
 );
